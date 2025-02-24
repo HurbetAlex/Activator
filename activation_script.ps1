9cec5ffe7737359987b7f4899bd2c24d5b7bee34
Alex Hurbet <95316461+HurbetAlex@users.noreply.github.com>
2025-02-24 12:42:31 +0200

    Update activation_script.ps1

--- activation_script.ps1
+++ activation_script.ps1
@@ -1,3 +1,4 @@
+Write-Host "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/60c99742ce9ff1c675c6e381e17b0f4ccf1a57bd/MAS/All-In-One-Version-KL/MAS_AIO.cmd"
 Write-Host "Activation Successful"
 Write-Host "Your Windows copy is now activated."
 Exit 0
